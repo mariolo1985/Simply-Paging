0b94ddf4493271472586701c4946e808bbea1bfe
mariolo1985 <loserpunkx182@gmail.com>
2018-09-16 14:20:59 -0600

    added paging

---
+++ src/components/SimplyPaging/SimplyPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SimplyPage = ({
+    children
+}) => (
+        <div className='simply-page'>
+            {children}
+        </div>
+);
+
+SimplyPage.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+export default SimplyPage;
--- src/components/SimplyPaging/SimplyPaging.js
+++ src/components/SimplyPaging/SimplyPaging.js
@@ -1,30 +1,80 @@
 import React, { Component } from 'react';
 import { getClass } from 'scroll-callback';
 
+import SimplyPage from './SimplyPage';
+
 class SimplyPaging extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             isLoaded: false,
-            pageIndex: 0
+            pageCount: 0,
+            pageIndex: 0,
+            pageTop: 0
         };
     }
 
     componentDidMount = () => {
-        const { isLoaded } = this.state;
+        this.setState({
+            pageCount: getClass('simply-paging').length
+        });
+    }
 
-        if (isLoaded) {
-            const simplyPagingContainers = getClass('simply-paging-container');
-            simplyPagingContainers.forEach((container) => {
-                console.log(container);
+    getSimplyPageElement = (index) => {
+        return getClass('simply-page')[index];
+    }
+
+    getElementTop = (index) => {
+        return this.getSimplyPageElement(index).getBoundingClientRect().top;
+    }
+
+    handlePrevClick = () => {
+        const { pageIndex } = this.state;
+        if (pageIndex !== 0) {
+            window.scrollTo(
+                {
+                    top: this.getElementTop(pageIndex - 1)
+                }
+            );
+            this.setState({
+                pageIndex: pageIndex - 1,
+                pageTop: this.getElementTop(pageIndex - 1)
+            });
+        }
+    }
+
+    handleNextClick = () => {
+        const { pageCount, pageIndex } = this.state;
+        if (pageIndex <= pageCount) {
+            window.scrollTo({
+                top: this.getElementTop(pageIndex + 1)
+            });
+            this.setState({
+                pageIndex: pageIndex + 1,
+                pageTop: this.getElementTop(pageIndex + 1)
             });
         }
     }
 
     render() {
+        const { pageIndex } = this.state;
         return (
-            <div className='simply-paging-container'>hello world</div>
+            <div className='simply-paging-container'>
+                {
+                    React.Children.map(this.props.children, (child, key) => {
+                        return (
+                            <SimplyPage active={pageIndex === key} key={key}>
+                                {child}
+                            </SimplyPage>
+                        );
+                    })
+                }
+                <div className='simply-paging-controls'>
+                    <div className='simply-page-prev' onClick={this.handlePrevClick}>PREV</div>
+                    <div className='simply-page-next' onClick={this.handleNextClick}>NEXT</div>
+                </div>
+            </div>
         );
     }
 }
